b1a19488d561460a4aa31cb0adf39ebb7175e395
Saba Antadze <sabantadze@gmail.com>
2023-11-26 14:40:04 -0800

    fixing bugs

--- components/multiple_elements/HiringCard.tsx
+++ components/multiple_elements/HiringCard.tsx
@@ -1,7 +1,4 @@
 "use client";
-
-import { fadeIn } from "@/utils/motion";
-import { motion } from "framer-motion";
 import Image from "next/image";
 import { Tilt } from "react-tilt";
 
@@ -14,7 +11,7 @@ type HiringCardProps = {
 const HiringCard = ({ title, index, icon }: HiringCardProps) => {
   return (
     <Tilt className="cursor-pointer">
-      <motion.div className="w-full xs:w-[250px] min-h-[280px] bg-green-90 px-8 py-12 rounded-3xl border border-blue-400 ">
+      <div className="w-full xs:w-[250px] min-h-[280px] bg-green-90 px-8 py-12 rounded-3xl border border-blue-400 ">
         <div className="flex flex-col justify-center items-center gap-8">
           <div className="p-4 bg-blue-400 rounded-full">
             <Image src={icon} width={20} height={20} alt={icon} />
@@ -23,7 +20,7 @@ const HiringCard = ({ title, index, icon }: HiringCardProps) => {
             {title}
           </h3>
         </div>
-      </motion.div>
+      </div>
     </Tilt>
   );
 };
